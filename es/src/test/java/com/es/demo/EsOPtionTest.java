2f642de57615cbe711c4a6659f1966e348ee8982
jializhong <jializhong@souche.com>
2022-09-26 20:36:51 +0800

    ElasticsearchOperations Api简单理解 做简单操作比较简单,但复杂操作需要用到的对象比较多

--- es/src/test/java/com/es/demo/EsOPtionTest.java
+++ es/src/test/java/com/es/demo/EsOPtionTest.java
@@ -4,32 +4,82 @@ import com.es.demo.entity.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.Query;
 
 /**
  * @author jlz
  * @date 2022年09月25日 23:58
  */
-public class EsOPtionTest extends DemoApplicationTests{
+public class EsOPtionTest extends DemoApplicationTests {
 
 
     private ElasticsearchOperations elasticsearchOperations;
 
     @Autowired
-    public EsOPtionTest(ElasticsearchOperations elasticsearchOperations){
+    public EsOPtionTest(ElasticsearchOperations elasticsearchOperations) {
         this.elasticsearchOperations = elasticsearchOperations;
     }
 
     /**
-     *
+     * 索引或一条文档
      */
     @Test
-    public void testIndex(){
+    public void testIndex() {
+        Product product = new Product();
+        product.setId(2);
+        product.setTitle("小浣熊干吃面");
+        product.setPrice(4.5);
+        product.setDescription("真好吃");
+
+        //id存在时更新 不存在时新增
+        elasticsearchOperations.save(product);
+    }
+
+    /**
+     * 通过id查询
+     */
+    @Test
+    public void testSearch() {
+        //id存在时更新 不存在时新增
+        Product product = elasticsearchOperations.get("1", Product.class);
+        System.out.println(product);
+    }
+
+
+    /**
+     * 查询全部
+     */
+    @Test
+    public void testSearchAll() {
+        //id存在时更新 不存在时新增
+        SearchHits<Product> search = elasticsearchOperations.search(Query.findAll(), Product.class);
+        System.out.println("总分数"+search.getMaxScore());
+        System.out.println("条数"+search.getTotalHits());
+
+        for (SearchHit<Product> productSearchHit : search) {
+            Product content = productSearchHit.getContent();
+            System.out.println(content);
+        }
+    }
+
+    /**
+     * 根据id删除
+     */
+    @Test
+    public void testDelete() {
+        //id存在时更新 不存在时新增
         Product product = new Product();
         product.setId(1);
-        product.setTitle("小浣熊干吃面");
-        product.setPrice(1.5);
-        product.setDescription("小浣熊干吃面真好吃");
+        elasticsearchOperations.delete(product);
+    }
 
-        elasticsearchOperations.save(product);
+    /**
+     * 删除全部
+     */
+    @Test
+    public void testDeleteAll() {
+        elasticsearchOperations.delete(Query.findAll(),Product.class);
     }
 }
